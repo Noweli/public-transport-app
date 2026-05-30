ed6a0479de640a0589089847a39cba48968e124b
Patryk Lewon <patryklewonn@gmail.com>
2026-05-30 16:35:32 +0200

    ApplicationDbContext.cs + LineStopConfiguration.cs

---
+++ backend/src/PublicTransport/PublicTransport.Infrastructure/Persistence/Configurations/LineStopConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PublicTransport.Core.Domain.Models.Entities;
+
+namespace PublicTransport.Infrastructure.Persistence.Configurations;
+
+public class LineStopConfiguration : IEntityTypeConfiguration<LineStop>
+{
+    public void Configure(EntityTypeBuilder<LineStop> builder)
+    {
+        builder.HasKey(lineStop => lineStop.Id);
+
+        builder.HasOne(lineStop => lineStop.Line)
+            .WithMany(line => line.LineStops)
+            .HasForeignKey(lineStop => lineStop.LineId);
+        
+        builder.HasOne(lineStop => lineStop.Stop)
+            .WithMany(stop => stop.LineStops)
+            .HasForeignKey(lineStop => lineStop.StopId);
+    }
+}
